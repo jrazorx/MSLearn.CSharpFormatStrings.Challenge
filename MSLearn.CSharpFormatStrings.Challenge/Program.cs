9118e68dad36c46a5126d927d57cf39e4e709829
jra <julien.gault.50@gmail.com>
2024-03-07 15:44:37 +0100

    added intro message

--- MSLearn.CSharpFormatStrings.Challenge/Program.cs
+++ MSLearn.CSharpFormatStrings.Challenge/Program.cs
@@ -12,12 +12,16 @@ string newProduct = "Glorious Future";
 decimal newReturn = 0.13125m;
 decimal newProfit = 63000000.0m;
 
-// Your logic here
+string newFinancialProductIntroMessage = "";
+newFinancialProductIntroMessage += $"Dear {customerName},\n";
+newFinancialProductIntroMessage += $"As a customer of our {currentProduct} offering we are excited to tell you about a new financial product that would dramatically increase your return.\n\n";
+newFinancialProductIntroMessage += $"Currently, you own {currentShares:N2} shares at a return of {currentReturn:P2}.\n\n";
+newFinancialProductIntroMessage += $"Our new product, {newProduct} offers a return of {newReturn:P2}.  Given your current volume, your potential profit would be {newProfit:C2}.\n";
+Console.WriteLine(newFinancialProductIntroMessage);
 
 Console.WriteLine("Here's a quick comparison:\n");
 
 string comparisonMessage = "";
 
-// Your logic here
 
 Console.WriteLine(comparisonMessage);
