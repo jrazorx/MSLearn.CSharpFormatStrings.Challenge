2f39af6ea1a14f182d03ad48f5e1dfea70f3b62f
jra <julien.gault.50@gmail.com>
2024-03-07 15:28:09 +0100

    Add project files.

---
+++ MSLearn.CSharpFormatStrings.Challenge/Program.cs
@@ -0,0 +1,20 @@
+﻿string customerName = "Ms. Barros";
+
+string currentProduct = "Magic Yield";
+int currentShares = 2975000;
+decimal currentReturn = 0.1275m;
+decimal currentProfit = 55000000.0m;
+
+string newProduct = "Glorious Future";
+decimal newReturn = 0.13125m;
+decimal newProfit = 63000000.0m;
+
+// Your logic here
+
+Console.WriteLine("Here's a quick comparison:\n");
+
+string comparisonMessage = "";
+
+// Your logic here
+
+Console.WriteLine(comparisonMessage);
